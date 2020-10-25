8daf78955510b176b13b82a3949edc32d2a49d92
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2020-10-25 06:52:15 +0200

    fit adding inquiries

--- routers/inquiries.routes.js
+++ routers/inquiries.routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {sendInquiry, getInquiries, getInquiryByStatus, getInquiryById, updateStatus} = 
 require('../controllers/inquiries.controller');
+const isAdmin = require('../middlewares/admin');
 
 router.post('/sendInquiry', sendInquiry);
-router.get('/allInquiries', getInquiries);
-router.get('/byStatus/:status', getInquiryByStatus);
-router.get('/byInquiryId/:id', getInquiryById);
-router.put('/updateStatus/inquiry/:id/status/:status', updateStatus)
+router.get('/allInquiries',[isAdmin], getInquiries);
+router.get('/byStatus/:status', [isAdmin],getInquiryByStatus);
+router.get('/byInquiryId/:id', [isAdmin],getInquiryById);
+router.put('/updateStatus/inquiry/:id/status/:status', [isAdmin],updateStatus)
 
 
 module.exports = router;
