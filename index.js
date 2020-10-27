6b0c53da7ab987e533f4ba18ea3ec64e1618eff2
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2020-10-27 19:47:57 +0200

    innn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config()
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 app.listen(port,()=>{
     console.log("App is running on port "+port);
     
