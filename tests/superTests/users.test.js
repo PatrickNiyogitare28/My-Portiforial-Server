fc9c5fe0afe26d8debd9046a3b6eacb16b463595
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2020-10-28 23:01:58 +0200

    chore adding tests

--- tests/superTests/users.test.js
+++ tests/superTests/users.test.js
@@ -1,11 +1,14 @@
 const app = require('../../app');
 const request = require('supertest');
 
-test('It should create a new user', async()=> {
-   await request(app).post('/api/users/signUp').send({
-       name: "test",
-       email: "test@gmail.com",
-       password: "Test@123"
-   })
-   .expect(201)
+describe('Test for singup', ()=> {
+   it('Should successfully signup a new user', async() => {
+    const res = await request(app).post('/api/users/signUp').send({
+        name: "test",
+        email: "test@gmail.com",
+        password: "Test@123"
+    })
+    .expect(res.statusCode()).toBe(201)
+   });
+  
 })
---
+++ tests/toTest/quotient.js
@@ -0,0 +1,3 @@
+exports.quotient = (num1,num2) => {
+    return num1/num2;
+}
